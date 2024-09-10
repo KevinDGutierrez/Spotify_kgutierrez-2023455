458ea0c69ca4afb3b9c2423f26aa8c9e98c98859
KevinDGutierrez <kgutierrez-2023455@kinal.edu.gt>
2024-09-10 10:34:40 -0600

    Foto artista y Cancion Agregadas

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -5,10 +5,12 @@ import { AudioPlayer } from './AudioPlayer';
 export const TrackList = ({ tracks, playTrack, artistImage, artistName }) => {
     const [currentTrack, setCurrentTrack] = useState(null);
     const [currentUrl, setCurrentUrl] = useState(null);
+    const [currentAlbumImage, setCurrentAlbumImage] = useState(artistImage); 
 
     const handlePlay = (track) => {
         setCurrentTrack(track.name); 
         setCurrentUrl(track.preview_url);
+        setCurrentAlbumImage(track.album_image);
         playTrack(track.preview_url);
     };
 
@@ -24,8 +26,8 @@ export const TrackList = ({ tracks, playTrack, artistImage, artistName }) => {
                         {tracks.map((track) => (
                             <div className="col-12 d-flex justify-content-between align-items-center mb-3" key={track.id}>
                                 <span className="text-dark fs-5">{track.name}</span>
-                                <button className="btn btn-link d-flex me-5"onClick={() => handlePlay(track)}style={{ padding: 0 }}>
-                                    <img src="https://i.ibb.co/rMVMtrb/play-removebg-preview.png" alt="Play"style={{ width: '50px', height: '50px' }}/>
+                                <button className="btn btn-link d-flex me-5" onClick={() => handlePlay(track)} style={{ padding: 0 }}>
+                                    <img src="https://i.ibb.co/rMVMtrb/play-removebg-preview.png" alt="Play" style={{ width: '50px', height: '50px' }}/>
                                 </button>
                             </div>
                         ))}
@@ -34,7 +36,7 @@ export const TrackList = ({ tracks, playTrack, artistImage, artistName }) => {
 
                 <div className="col-md-5 position-relative">
                     <div className="card" style={{ width: '25rem', backgroundColor: 'rgba(0, 0, 0, 0.7)', color: 'white'}}>
-                        <img src={artistImage || "https://i.ibb.co/Hgmgfgs/Logo-Spotify.jpg"} className="card-img-top" alt="Artist"/>
+                        <img src={currentAlbumImage || "https://i.ibb.co/Hgmgfgs/Logo-Spotify.jpg"} className="card-img-top" alt="Album"/>
                         <div className="card-body">
                             <h1 className="card-title">{currentTrack || 'Sin Reproducciones'}</h1>
                             <AudioPlayer trackName={currentTrack} url={currentUrl} />
--- src/services/spotify.jsx
+++ src/services/spotify.jsx
@@ -1,6 +1,6 @@
 const CLIENT_ID = '7cf755e6bdeb458ab722b97b59402735';
 const CLIENT_SECRET = 'eff0da00ccce49cfa3858b9f6859ca38';
-const TOKEN = 'BQCyFCNIBw34xi92CDHSd3HimX0Ka-LldJCUzK2sDRAF1kBbu19AK8c1-NnW7IL4cdJmIdFVDKBs64wuTLRMfwGV2K80qaLUufpKMtb9grMX17r0LgM';
+const TOKEN = 'BQDRL6T117RdA3Zk8jXtz5TJYWLdOgOikb0BtHq8_N7ehVCqhLkvOIyZrsI2-t5T6grrCPXGto1Lws9CZCWlNpHSHM-cY72BsWHFVaXR29CvhzfUnC8';
 
 const BASE_URL = 'https://api.spotify.com/v1';
 
@@ -17,5 +17,12 @@ export const searchArtistAPI = async (artistName) => {
 export const getArtistTracksAPI = async (artistId) => {
     const resp = await fetch(`${BASE_URL}/artists/${artistId}/top-tracks?market=US`, { headers });
     const data = await resp.json();
-    return data.tracks;
+    return data.tracks.map(track => ({
+        id: track.id,
+        name: track.name,
+        preview_url: track.preview_url,
+        album_image: track.album.images[0]?.url || '', 
+    }));
 };
+
+
